c2a40e84ee26ab5d2d100c74168ea8100b85c534
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-06 15:30:47 -0500

    actividad terminada

--- usereducer/src/CompraB.jsx
+++ usereducer/src/CompraB.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer, useRef, useState } from 'react'
 
 const types = {
   menos: 'menos',
@@ -9,7 +9,7 @@ const types = {
 
 
 function CompraB() {
-  const valorInicial = [{id:1, nombre: 'Agua', cantidad: 3}, {id:2, nombre: 'Pan', cantidad: 2}]
+  const valorInicial = []
   const reducer = (state, action)=> {
     switch(action.type){
       case types.menos:
@@ -26,16 +26,27 @@ function CompraB() {
         })
       case types.eliminar:
         return state.filter(valor => valor.id !== action.payload )
+      case types.comprar:
+        return [...state, action.payload]
+
+      default:
+        return state;
     }
     return state;
   }
   const [lista, dispatch] = useReducer(reducer, valorInicial)
   const [miProducto, setmiProducto] = useState('');
+  const inputName = useRef(null);
   return (
     <>
       <label htmlFor='producto'>Producto: </label>
-      <input id='producto' value={miProducto} onChange={(e)=> setmiProducto(e.target.value)}/>
-      <button>Añadir</button>
+      <input id='producto' ref={inputName} value={miProducto} onChange={(e)=> setmiProducto(e.target.value)}/>
+      <button onClick={()=>
+      { inputName.current.focus();
+      setmiProducto('')
+        dispatch({type: types.comprar, 
+        payload: {id: Date.now(), nombre: miProducto, cantidad: 0} })}
+        }>Añadir</button>
       {
         lista.map((producto=>{
           return <div key={producto.id}>
