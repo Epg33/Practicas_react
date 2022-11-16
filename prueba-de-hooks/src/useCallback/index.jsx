bf6de5f48baa3c9729eb55893bc892bc2e9c8a4f
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-16 14:43:45 -0500

    revisar

--- prueba-de-hooks/src/useCallback/index.jsx
+++ prueba-de-hooks/src/useCallback/index.jsx
@@ -1,10 +1,23 @@
-import React, {useCallback} from 'react'
+import React, { useCallback, useRef, useState } from "react";
 
 function Index() {
-  
+  const [count, setCount] = useState([]);
+  const num = useRef();
+  const add = () => {
+    console.log(num.current.value);
+    setCount((c) => [...c, num.current.value]);
+  };
   return (
-    <div>index</div>
-  )
+    <>
+      <div style={{display:'flex', flexDirection:'column'}}>
+        <div>{count.map(c=>{
+          return <div>{c}</div>
+        })}</div>
+        <input type="number" ref={num} />
+        <button onClick={add}>+</button>
+      </div>
+    </>
+  );
 }
 
-export default Index
+export default Index;
