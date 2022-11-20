fbe19cef01dd448e829b4351f7d22a6ef116fc7c
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-20 15:06:05 -0500

    fetching

--- next-js-13-app/app/posts/page.jsx
+++ next-js-13-app/app/posts/page.jsx
@@ -1,5 +1,20 @@
-export default function PostsPage () {
+const fetchPosts = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+}
+
+export default async function PostsPage ({ params }) {
+  const posts = await fetchPosts()
   return (
-    <h1>Aqui tenemos los Posts...</h1>
+    <section>
+      {posts.map(post => {
+        return (
+          <article key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </article>
+        )
+      })}
+    </section>
   )
 }
