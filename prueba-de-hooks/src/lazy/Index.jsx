1f117b0d9496ecb13befd48f28a3130860392695
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-21 14:40:18 -0500

    lazy

--- prueba-de-hooks/src/lazy/Index.jsx
+++ prueba-de-hooks/src/lazy/Index.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {Suspense, lazy, useState} from 'react'
+import skuli from './skull.jpg'
+
+const Something = lazy(()=>import('./Something.jsx'))
+const bone = lazy(()=>import('./bone.jfif'))
+const skull = lazy(()=>import('./skull.jpg'))
 
 function Index() {
   return (
-    <div>Index</div>
+    <>
+      <Suspense fallback={<h1>waiting...</h1>}>
+        <Something />
+        <img src={bone} alt='a bone for lazy loading'/>
+        {bone}
+        <img src={skuli} alt='idk something bro' />
+        {skuli}
+      </Suspense>
+    </>
   )
 }
 
