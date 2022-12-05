e1d40ca8bf30847bac5a8223d99906fe9d61afbc
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-05 11:10:40 -0500

    szs

--- news-app/pages/index.js
+++ news-app/pages/index.js
@@ -6,20 +6,12 @@ import { useRouter } from "next/router";
 import PageLayout from "../components/PageLayout";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(()=>{
-    fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-11-02&sortBy=published&apiKey=b5dbd67693af4df080df4680eb8b164b')
-      .then(res => res.json())
-      .then(response => {
-        setArticles(response.articles)
-      })
-  }, [])
+export default function Home({ articles }) {
+  
   return (
     <PageLayout title="NewsApp - Home">
       <div className={styles.container}>
-        {articles.length=== 0 && <p>Loading...</p>}
+        {articles.length=== 0 && <p>No tenemos articulos</p>}
         {articles.length > 0 && articles.map((article, index) => {
           return <article key={index}>
             <img alt={`Image for the article ${article.title}`} src={article.urlToImage} />
@@ -30,4 +22,14 @@ export default function Home() {
       </div>
     </PageLayout>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-12-02&sortBy=published&apiKey=b5dbd67693af4df080df4680eb8b164b')
+  const {articles} = await response.json()
+  return {
+    props: {
+      articles
+    }
+  }
 }
