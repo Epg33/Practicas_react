24c70ae7c1e5c6146d60ec3552e759463dbc031d
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-12-17 20:54:41 -0500

    swr

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,50 +1,16 @@
-import "./App.css";
-import "./micss.css";
-import imagen0 from "./imagenes/rey_atanagildo.png";
-import imagen1 from "./imagenes/rey_leogivildo.png";
-import imagen2 from "./imagenes/rey_sisebuto.png";
-import imagen3 from "./imagenes/rey_incognito.png";
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom'
+import Clase from './clase'
+import PruebaSwr from './PruebaSwr'
 
 function App() {
-  let nombres = ["Atanagildo", "Leogivildo", "Sisebuto"];
-  function cambiar(e) {
-    if (e.target.src.includes("incognito")) {
-      e.target.style.visibility = "hidden";
-    } else {
-      e.target.src = imagen3;
-    }
-    e.target.parentNode.style.backgroundColor = "white";
-    e.target.parentNode.style.border = "none";
-  }
-  function texto(k) {
-    if (k.target.innerHTML == "visto") {
-      k.target.innerHTML = "";
-    } else {
-      k.target.innerHTML = "visto";
-    }
-  }
   return (
-    <div className="contenedor">
-      <div className="caja">
-        <img src={imagen0} onClick={cambiar} />
-        <div className="nombre" onClick={texto}>
-          {nombres[0]}
-        </div>
-      </div>
-      <div className="caja">
-        <img src={imagen1} onClick={cambiar} />
-        <div className="nombre" onClick={texto}>
-          {nombres[1]}
-        </div>
-      </div>
-      <div className="caja">
-        <img src={imagen2} onClick={cambiar} />
-        <div className="nombre" onClick={texto}>
-          {nombres[2]}
-        </div>
-      </div>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Clase />}></Route>
+        <Route path='/swr' element={<PruebaSwr />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ mi-app/src/PruebaSwr.jsx
@@ -0,0 +1,41 @@
+/*
+refreshInterval
+dedupingInterval
+revalidateOnFocus
+RevalidateOnReconnect
+refreshWhenOffline
+*/
+import axios from "axios"
+import useSWR from 'swr'
+
+const PruebaSwr = () => {
+  const {data, error} = useSWR(
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=1",
+    async(url) => {
+      const response = await axios(url);
+      return response;
+    }
+  )
+  
+  console.log(data);
+
+  if(error) return <div>failed to load</div>
+  if(!data) return <div>Loading...</div>
+  return (
+    <>
+      <main style={{paddingLeft: '15px',display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '15px'}}>
+      {
+        data.data.results.map(movie => {
+          return <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img style={{width: '30%', height: 'auto'}} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} alt='movie cover' />
+            <spas>rate: {movie.vote_average}</spas>
+          </div>
+        })
+      }
+      </main>
+    </>
+  )
+}
+
+export default PruebaSwr
