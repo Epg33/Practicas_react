c4961675fbd0e38bf3d70cd18745bf5817afa502
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-01-05 13:45:13 -0500

    hacer infite scroll

--- query-ts-tailwind-practice/src/pages/list/List.tsx
+++ query-ts-tailwind-practice/src/pages/list/List.tsx
@@ -1,16 +1,19 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
-import { Key } from "react";
+import { Key, useState } from "react";
 import { getList } from "../../services";
 import { ListItem } from "../../types";
 
 export interface ListComponent {}
 
 const List: React.FC<ListComponent> = (): JSX.Element => {
+  const [page, setPage] = useState<number>(0);
   const { isLoading, error, data }: UseQueryResult<AxiosResponse<any, any>> =
     useQuery({
-      queryKey: ["list"],
-      queryFn: getList,
+      queryKey: ["list", page],
+      queryFn: ()=> getList(page),
+      refetchOnWindowFocus: false,
+      keepPreviousData: true
     });
 
   if (isLoading) { return <h1>Loading... {isLoading}</h1>; }
@@ -19,9 +22,12 @@ const List: React.FC<ListComponent> = (): JSX.Element => {
     <>
       <section className="grid grid-cols-3 place-items-center">
         {data?.data.map((pic:ListItem, index:Key ) => {
-          return <img key={index} src={`https://picsum.photos/id/${pic.id}/400/250`} />;
+          return <img key={index} src={`https://picsum.photos/id/${pic.id}/400/250`} alt={`picture taked by ${pic.author}`}/>;
         })}
       </section>
+      <div className="w-screen flex justify-center">
+        <button onClick={()=>setPage(page+1)}>Ver mas</button>
+      </div>
     </>
   );
 };
--- query-ts-tailwind-practice/src/services/Queries.ts
+++ query-ts-tailwind-practice/src/services/Queries.ts
@@ -1,7 +1,13 @@
 import axios, { Axios, AxiosResponse } from "axios"
 
-export const getList = async ():Promise<AxiosResponse<any, any>> => {
-  const data = await axios.get('https://picsum.photos/v2/list?page=1&limit=30')
-  console.log(data);
-  return data;
+export const getList = async (page:Number):Promise<AxiosResponse<any, any>> => {
+  try{
+    const res = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=30`)
+    console.log(res);
+    return res;
+  }
+  catch(err:any){
+    console.log(err)
+    return err
+  }
 }
