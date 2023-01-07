339796d1eb21c14ced45e8df7d0fe7d807cd0aff
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-01-07 16:14:22 -0500

    arreglar

--- query-ts-tailwind-practice/src/pages/random/Random.tsx
+++ query-ts-tailwind-practice/src/pages/random/Random.tsx
@@ -1,5 +1,5 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Loading } from '../../components'
 import { getRandomImage } from '../../services';
 
@@ -11,9 +11,10 @@ const Random:React.FC<RandomComponent> = ():JSX.Element => {
     queryKey: ['rand', id],
     queryFn: ()=> getRandomImage(id)
   })
+  const index:number = id.toString().length===4 ? 29 : id.toString().length===3 ? 28 : id.toString().length===2 ? 27 : 26
+  const [imageUrl, setImageUrl] = useState<string>(data?.data.download_url)
   const gray = useRef<HTMLInputElement>(null);
   const blur = useRef<HTMLInputElement>(null)
-  const index:number = id.toString().length>3 ? 29 : 28
   const generateRandomNumber = ():void => {
     const number:number = Math.floor(Math.random()*(1084 + 1)) 
     setId(number===934 ? 1 : number);
@@ -21,12 +22,11 @@ const Random:React.FC<RandomComponent> = ():JSX.Element => {
 
   if(isLoading) return <div className='w-full h-60 grid place-content-center'><Loading /></div>
   if(error) return <h1>Error while fetching</h1>
-
   return (
     <div>
-      <img src={`${(data?.data.download_url as string).split('').splice(0, index).join('')}/500/400`} alt="random image from lorem pictum" />
+      <img src={data?.data.download_url} alt="random image from lorem pictum" />
       <label htmlFor="gray">Escala de grises</label><input type="checkbox" ref={gray} name="gray" id="" />
-      <label htmlFor="blur">Aplicar blur</label><input type="checkbox" ref={blur} name="blur" id="" />
+      <label htmlFor="blur">Aplicar blur</label><input type="checkbox" ref={blur}  name="blur" id="" />
       <button onClick={()=>{generateRandomNumber()}}>Generar imagen aleatoria</button>
     </div>
   )
