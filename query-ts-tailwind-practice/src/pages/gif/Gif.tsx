8d7c86f9633a12efd0c5675c711c257acfe19f93
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-01-11 15:02:53 -0500

    semi arreglado

--- query-ts-tailwind-practice/src/pages/gif/Gif.tsx
+++ query-ts-tailwind-practice/src/pages/gif/Gif.tsx
@@ -1,19 +1,23 @@
 import { useQuery } from "@tanstack/react-query"
+import { useState } from "react"
 import { Loading } from "../../components"
 import { getRandomGif } from "../../services"
 
 export interface GifComponent {}
 
 const Gif:React.FC<GifComponent> = ():JSX.Element => {
-  const {data:gif , error, isLoading } = useQuery({
-    queryKey: ['randomGif'],
-    queryFn: getRandomGif
+  const [revalidate, setRevalidate] = useState<boolean>(false)
+  const {data , error, isLoading } = useQuery({
+    queryKey: ['randomGif', revalidate],
+    queryFn: getRandomGif,
+    refetchOnWindowFocus: false
   })
   if(isLoading) return <div className='w-full h-screen grid place-items-center'><Loading /></div>
   if(error) return <h1>Error while fetching...</h1>
   return (
     <>
-      <iframe src={gif?.data.data.embed_url} width={`${gif?.data.data.images.original.width}`} height={`${gif?.data.data.images.original.height}`} frameBorder="0" allowFullScreen></iframe>
+      <iframe src={data?.data.data.embed_url} width={`${data?.data.data.images.original.width}`} height={`${data?.data.data.images.original.height}`} frameBorder="0" allowFullScreen></iframe>
+      <button onClick={()=>setRevalidate(!revalidate)}>Random gif</button>
     </>
   )
 }
