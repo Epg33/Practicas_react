8ddf56a76e37019f70ef5c98e3c24d5fbad6e825
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-03-12 18:44:20 -0500

    szs

--- framer-prueba/src/App.tsx
+++ framer-prueba/src/App.tsx
@@ -1,29 +1,37 @@
 import {motion} from 'framer-motion'
+import { useState } from 'react'
 
 const variants = {
   odd: {
-    backgroundColor: '#333',
-    color: '#aaa'
+    backgroundColor: '#ff0000',
+    color: '#000',
+    scale: 0.8,
+    transition: {
+      duration: 2
+    }
   },
   even: {
-    backgroundColor: '#999',
-    color: '#000'
+    backgroundColor: '#003333',
+    color: '#fff',
+    scale: 1.1,
+    transition: {
+      duration: 1
+    }
   }
 }
 function App() {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <>
-      <main className='w-full h-[200vh] flex justify-center pt-[120vh] bg-cyan-500'>
+      <main className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-cyan-500'>
         <motion.div 
-          initial={{scale: 1}} 
-          transition={{duration: 2}} 
-          whileInView={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 270, 270, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"]
-          }} 
-          className='h-28 w-28 rounded-md bg-slate-100'></motion.div>
-        {/* <motion.h1 initial={{color: '#000'}} transition={{duration: 1.5}} animate={{y: 100, color: '#eee'}} className='text-slate-100 font-bold text-2xl h-fit'>Hello world</motion.h1> */}
+          drag dragConstraints={{top: -50, left: -50, right: 50, bottom: 50}}
+          className='h-28 w-28 rounded-md bg-slate-100 grid place-content-center text-2xl'></motion.div>
+        {/* <motion.div 
+          animate={counter%2===0 ? 'even' : 'odd'}
+          variants={variants}
+          className='h-28 w-28 rounded-md bg-slate-100 grid place-content-center text-2xl'>{counter}</motion.div>
+        <button onClick={()=>setCounter(counter+1)} className='w-28 bg-slate-100 rounded-md '>Dale!</button> */}
       </main>
     </>
   )
