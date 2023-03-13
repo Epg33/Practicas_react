ad8ff9e6cb8fea005a736618440fc0199bd6ceb2
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-03-12 20:04:56 -0500

    szs

--- framer-prueba/src/App.tsx
+++ framer-prueba/src/App.tsx
@@ -1,5 +1,6 @@
-import {motion} from 'framer-motion'
+import { motion, useTransform, useMotionValue } from 'framer-motion'
 import { useState } from 'react'
+
 
 const variants = {
   odd: {
@@ -19,15 +20,24 @@ const variants = {
     }
   }
 }
+
 function App() {
-  const [counter, setCounter] = useState<number>(0);
+  // const y = useMotionValue<number>(0)
+  // const bgColor = useTransform(y, [-100, 0, 100], ['#fff', '#999', '#000']);
+  const [align, setAlign] = useState<string>('left');
   return (
     <>
       <main className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-cyan-500'>
-        <motion.div 
-          drag dragConstraints={{top: -50, left: -50, right: 50, bottom: 50}}
+        <motion.h1 layout>
+          <p style={{textAlign: align}}>Hola</p>
+        </motion.h1>
+        <button onClick={()=>setAlign('right')} className='w-28 bg-slate-100 rounded-md'>change align text</button>
+        {/*<motion.div 
+          drag='y'
+          style={{backgroundColor: bgColor, y}}
+          dragConstraints={{top: -100, bottom: 100}}
           className='h-28 w-28 rounded-md bg-slate-100 grid place-content-center text-2xl'></motion.div>
-        {/* <motion.div 
+         <motion.div 
           animate={counter%2===0 ? 'even' : 'odd'}
           variants={variants}
           className='h-28 w-28 rounded-md bg-slate-100 grid place-content-center text-2xl'>{counter}</motion.div>
