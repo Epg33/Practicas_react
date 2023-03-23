ca85bb9a4e9af8bc37e617f9dd97d8200626767a
Ethiem Guerrero <ethiemalex03@gmail.com>
2023-03-23 13:01:02 -0500

    editar y eliminar

--- redux_task/src/components/TaskForm.tsx
+++ redux_task/src/components/TaskForm.tsx
@@ -1,16 +1,18 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { addTask, TaskT } from '../features/tasks/taskSlice'
-import { useNavigate } from 'react-router-dom'
+import { addTask, editTask, TaskT } from '../features/tasks/taskSlice'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const TaskForm = () => {
   const navigate = useNavigate()
   const tasks: TaskT[] = useSelector( state => state.tasks)
   const[task, setTask] = useState({
+    id: 0,
     title: '',
-    description: ''
+    description: '',
+    completed: false
   })
-
+  const params = useParams()
   const dispatch = useDispatch()
 
   const handleChange = (e: React.ChangeEvent<any>) => {
@@ -22,16 +24,26 @@ const TaskForm = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(addTask({
-      ...task,
-      id: tasks[tasks.length-1].id+1
-    }))
+    if(params.id){
+      dispatch(editTask(task))
+    } else {
+      dispatch(addTask({
+        ...task,
+        id: tasks[tasks.length-1].id+1
+      }))
+    }
     navigate('/')
   }
+
+  useEffect(()=> {
+    if(params.id) {
+      setTask(tasks.find( tsk => tsk.id === Number(params.id)))
+    }
+  }, [])
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="title" id="" placeholder='title' onChange={handleChange}/>
-      <textarea name="description" placeholder='description' id="" onChange={handleChange}></textarea>
+      <input type="text" name="title" id="" placeholder='title' onChange={handleChange} value={task.title}/>
+      <textarea name="description" placeholder='description' id="" onChange={handleChange} value={task.description}></textarea>
       <input type="submit" value="save" />
     </form>
   )
